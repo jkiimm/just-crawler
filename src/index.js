bc784024966493e437ebf1477f2df7d89d340cca
Jinyoung Kim <ligeek49@gmail.com>
2015-06-08 21:49:02 +0900

    edit for ajouin

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ var childProcess = require('child_process');
 var phantomjs = require('phantomjs');
 var binPath = phantomjs.path;
 var glob = require('glob');
-var ttsMaker = require('./tts-maker');
+//var ttsMaker = require('./tts-maker');
 
 var auth = {
     id: process.argv[2],
-    pw: process.argv[3]'
+    pw: process.argv[3]
 };
 
-var urls = [
-  'http://m.cafe.naver.com/ArticleList.nhn?search.clubid=13227349&search.menuid=536&search.boardtype=L',
-  'http://m.cafe.naver.com/ArticleList.nhn?search.clubid=13227349&search.menuid=537&search.boardtype=L',
-  'http://m.cafe.naver.com/ArticleList.nhn?search.clubid=13227349&search.menuid=538&search.boardtype=L',
-  'http://m.cafe.naver.com/ArticleList.nhn?search.clubid=13227349&search.menuid=539&search.boardtype=L'
-];
+var url = 'https://ajouin.com/lecturereview1';
 
 var childArgs = [
   path.join(__dirname, 'opic-crawler.js'),
@@ -23,11 +18,8 @@ var childArgs = [
   auth.pw
 ];
 
-urls.forEach(function(url) {
-  console.log(url);
-  childProcess.execFile(binPath, childArgs.concat(url), function(err, stdout, stderr) {
-    console.log(err, stdout, stderr); 
-  });
+childProcess.execFile(binPath, childArgs.concat(url), function(err, stdout, stderr) {
+  console.log(err, stdout, stderr); 
 });
 
 //glob('./outputs/*.txt', function(err, files) {
--- src/opic-crawler.js
+++ src/opic-crawler.js
@@ -1,112 +1,121 @@
-var page = require('webpage').create();
-var fs = require('fs');
-var system = require('system');
-var args = system.args;
-console.log(args);
+(function() {
+  var page = require('webpage').create();
+  var fs = require('fs');
+  var system = require('system');
+  var args = system.args;
+  console.log(args);
 
-var stepBunch = [{
-  url: 'https://nid.naver.com/nidlogin.login',
-  todo: function() {
-    page.evaluate(function(id, pw) {
-      var id = document.querySelector('#id').value = id;
-      pw = document.querySelector('#pw').value = pw;
-      document.querySelector('#frmNIDLogin').submit();
-    }, args[1], args[2]);
-  },
-}, {
-  url: args[3],
-  todo: function() {
-    page.evaluate(function() {
-      // Javascript in Web
-      var target = null,
-      list = document.querySelectorAll('.lst4'); 
-      
-      for(var i = 0; i < list.length; i++) {
-        if(list[i].className === 'lst4') {
-          target = list[i].querySelector('li a');
-          break;
-        }
-      }
+  var stepBunch = [{
+    url: 'https://ajouin.com',
+    todo: function() {
+      page.evaluate(function(id, pw) {
+        document.querySelector('.idpw input[name="user_id"]').value = id;
+        document.querySelector('.idpw input[name="password"]').value = pw;
 
-      if(target) {
-        var ev = document.createEvent("MouseEvents");
-        ev.initEvent("click", true, true);
-        target.dispatchEvent(ev);
-      }
-      return null;
-    });
-
-    var questions = [],
-    getQuestion = function() {
-      var content = page.evaluate(function() {
-        return document.querySelector('#postContent').textContent; 
-      });
-      var str = content.match(/[a-zA-Z'].[ ,.a-zA-Z?!\n]*/g);
-      console.log(str);
-      return str.filter(function(el) { return el.length > 20; }).join(' ');
+        document.querySelector('#fo_login_widget').submit();
+      }, args[1], args[2]);
     },
-    getTitle = function() {
-      return page.evaluate(function() {
-        return document.querySelector('.post_tit h2').innerHTML.trim();
-      }); 
-    },
-    navigateNext = function() {
-      return page.evaluate(function() {
-        var target = document.querySelector('.post_nav .next');
+  }, {
+    url: args[3],
+    todo: function() {
+      page.evaluate(function() {
+        // Javascript in Web
+        var target = null,
+          list = document.querySelector('tbody'); 
+
+        target = list.querySelector('tr .title a');
+        //target = list.querySelector('tr .title a');
+        //      for(var i = 0; i < list.length; i++) {
+        //        if(list[i].className === '.bd_lst_wrp tbody') {
+        //          target = list[i].querySelector('.title a');
+        //          break;
+        //        }
+        //      }
+
         if(target) {
           var ev = document.createEvent("MouseEvents");
           ev.initEvent("click", true, true);
           target.dispatchEvent(ev);
-          return true;
         }
-        return false;
+        return null;
       });
+
+      var questions = [],
+        getQuestion = function() {
+        var content = page.evaluate(function() {
+          return document.querySelector('article').textContent; 
+        });
+        console.log(content);
+        return content;
+      },
+      getTitle = function() {
+        return page.evaluate(function() {
+          return document.querySelector('.np_18px a').innerHTML.trim();
+          //return document.querySelector('tbody  a').innerHTML.trim();
+        }); 
+      },
+      navigateNext = function() {
+        return page.evaluate(function() {
+          // var curNo = document.querySelector('.bd_lst_wrp tbody .select');
+          var curNo = document.querySelector('tr.select + tr');
+          var target = curNo.querySelector('.title a');
+          if(target) {
+            var ev = document.createEvent("MouseEvents");
+            ev.initEvent("click", true, true);
+            target.dispatchEvent(ev);
+            return true;
+          }
+          return false;
+        });
+      },
+      fileWrite = function(strArr) {
+        var qid = Math.floor(Math.random()*1000);
+        //var qid = crypto.randomBytes(4).toString('hex');
+        var text = strArr.join('\n\n');
+        fs.write('./outputs/q_'+qid+'.txt', text, 'w');
+        console.log('*************************** File was saved ***************************');
+      };
+
+      for(var i = 1; i <= 1; i++) {
+        (function(ms) {
+          setTimeout(function() {
+            try {
+              //console.log(getTitle());
+              questions.push(getQuestion());
+            } catch(e) {
+              phantom.exit();
+            }
+            if(!navigateNext()) {
+              fileWrite(questions.reverse());
+              phantom.exit();
+            }
+          }, ms*1000);
+        })(i);
+      }
+
     },
-    fileWrite = function(strArr) {
-      var qid = Math.floor(Math.random()*1000);
-      //var qid = crypto.randomBytes(4).toString('hex');
-      var text = strArr.join('\n\n');
-      fs.write('./outputs/q_'+qid+'.txt', text, 'w');
-      console.log('*************************** File was saved ***************************');
-    };
+  }];
 
-    for(var i = 1; i <= 100; i++) {
-      (function(ms) {
-        setTimeout(function() {
-          try {
-            console.log(getTitle());
-            questions.push(getQuestion());
-          } catch(e) {
-            phantom.exit();
-          }
-          if(!navigateNext()) {
-            fileWrite(questions.reverse());
-            phantom.exit();
-          }
-        }, ms*250);
-      })(i);
-    }
+  function handlePage(unit) {
+    if(!unit) { return; }
 
-  },
-}];
+    page.open(unit.url, function(status) {
+      console.log(unit.url);
+      console.log('STATUS: ' + status);
+      //page.render(Math.floor(Math.random()*1000) + '.png');
 
-function handlePage(unit) {
-  if(!unit) { return; }
+      if(status === 'success') {
+        unit.todo();
+      }
+      //page.render(Math.floor(Math.random()*1000) + '.png');
+      window.setTimeout(nextPage, 2500); 
+    });
+  }
 
-  page.open(unit.url, function(status) {
-    console.log(unit.url);
-    console.log('STATUS: ' + status);
+  function nextPage() {
+    var stepUnit = stepBunch.shift();
+    handlePage(stepUnit);
+  }
 
-    if(status === 'success') {
-      unit.todo();
-    }
-    window.setTimeout(nextPage, 2000); 
-  });
-}
-
-function nextPage() {
-  var stepUnit = stepBunch.shift();
-  handlePage(stepUnit);
-}
-
-nextPage();
+  nextPage();
+})();
